ba731d551af6ad1d8cd9895230548d26a2166bdc
Emiliano Romero <a01028415@tec.mx>
2024-10-01 19:40:39 -0600

    prueba

--- Client/donorhub/src/App.tsx
+++ Client/donorhub/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Admin, Resource } from 'react-admin'; // Add these imports
+import jsonServerProvider from 'ra-data-json-server'; // Import the JSON server provider
 import Login from './views/login';
-import Donations from './views/Donations';
+import { DonationList, DonationEdit, DonationCreate } from './views/Donations';
 import { isAuthenticated } from './Utilities/authUtils';
+
+const dataProvider = jsonServerProvider('http://localhost:3000'); // Set up the data provider
 
 const ProtectedRoute: React.FC<{ element: React.ReactElement }> = ({ element }) => {
   return isAuthenticated() ? element : <Navigate to="/login" replace />;
@@ -12,9 +16,14 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/admin" element={
+          <Admin dataProvider={dataProvider}>
+            <Resource name="donations" list={DonationList} edit={DonationEdit} create={DonationCreate} />
+          </Admin>
+        } />
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/donations" element={<ProtectedRoute element={<Donations />} />} />
+        <Route path="/donations" element={<ProtectedRoute element={<DonationList />} />} />
       </Routes>
     </Router>
   );
--- Client/donorhub/src/views/Donations.tsx
+++ Client/donorhub/src/views/Donations.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import {List, required, SimpleList, Datagrid, TextField, NumberField, DateField, Filter, EditButton, Edit, SimpleForm, TextInput, NumberInput, DateInput, Create, FilterProps} from "react-admin";
+import {useMediaQuery, Theme} from "@mui/material";
 
-const Donations: React.FC = () => {
-  return <div>Donations Component</div>;
-};
+const DonationFilter = (props: FilterProps) => (
+    <Filter {...props}>
+        <TextInput label="Search by donor name" source="donorName" alwaysOn />
+    </Filter>
+);
 
-export default Donations;
+export const DonationList = () => {
+    const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+    return (
+        <List>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={(record) => record.donorName}
+                    secondaryText={(record) => record.amount}
+                    tertiaryText={(record) => record.date}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <TextField source="donorName" />
+                    <NumberField source="amount" />
+                    <DateField source="date" />
+                    <TextField source="message" />
+                    <EditButton />
+                </Datagrid>
+            )}
+        </List>
+    );
+}
+
+export const DonationEdit = () => {
+    return (
+        <Edit>
+            <SimpleForm>
+                <TextInput source="id" InputProps={{ disabled: true }} />
+                <TextInput source="donorName" />
+                <NumberInput source="amount" />
+                <DateInput source="date" />
+                <TextInput source="message" />
+            </SimpleForm>
+        </Edit>
+    )
+}
+
+export const DonationCreate = () => {
+    return (
+        <Create>
+            <SimpleForm>
+                <TextInput source="id" validate={[required()]} />
+                <TextInput source="donorName" />
+                <NumberInput source="amount" />
+                <DateInput source="date" />
+                <TextInput source="message" />
+            </SimpleForm>
+        </Create>
+    )
+}
